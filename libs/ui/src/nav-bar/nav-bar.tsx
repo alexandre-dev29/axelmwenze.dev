71aae06db730da3224e9dcf20715fec3de5e7d10
alexandre-dev29 <axel.business29@gmail.com>
2022-08-02 17:27:14 +0200

    first part of navbar

--- libs/ui/src/nav-bar/nav-bar.tsx
+++ libs/ui/src/nav-bar/nav-bar.tsx
@@ -1,12 +1,36 @@
 import styles from './nav-bar.module.scss';
+import Link from 'next/link';
 
 /* eslint-disable-next-line */
 export interface NavBarProps {}
 
+const listLinks: Array<{ linkName: string; link: string }> = [
+  { link: '/', linkName: 'Home' },
+  { link: '/about', linkName: 'About' },
+  { link: '/work', linkName: 'Work' },
+  { link: '/skills', linkName: 'Skills' },
+  { link: '/contact', linkName: 'Contact' },
+];
+
 export function NavBar(props: NavBarProps) {
   return (
-    <div className={'top-0'}>
-      <h1>Welcome to NavBar!</h1>
+    <div className={'top-0 flex h-[7vh] backdrop-blur-2xl '}>
+      <h1 className={'p-0 m-0 text-2xl w-[10vw] my-auto pl-4 text-violet-600'}>
+        Axel <span className={'text-blue-400'}> Mwenze</span>
+      </h1>
+      <div className={'flex flex-1  p-0 justify-center items-center'}>
+        <ul
+          className={`grid grid-cols-${listLinks.length} justify-center items-center m-0`}
+        >
+          {listLinks.map(({ linkName, link }, index) => (
+            <li key={index} className={'m-0'}>
+              <Link href={link}>
+                <p className={'p-4 cursor-pointer'}>{linkName}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
